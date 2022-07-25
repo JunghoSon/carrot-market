84c8b8579d3267ce154e8886ee9b58ad3e806eaa
“손정호/AI솔루션개발팀/SKP” <jhson@sk.com>
2022-07-25 18:55:47 +0900

    미들웨어 수정

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -6,13 +6,14 @@ export function middleware(req: NextRequest, ev: NextFetchEvent) {
     console.log(1);
     return new Response("Plz don't be a bot. Be human.", { status: 403 });
   }
-  if (req.ua?.isBot !== undefined) {
-    if (!req.url.includes("/api")) {
-      if (!req.url.includes("/enter") && !req.cookies.carrotsession) {
-        return NextResponse.redirect(`${req.nextUrl.origin}/enter`);
-      }
+  // if (req.ua?.isBot !== undefined) {
+  if (!req.url.includes("/api")) {
+    if (!req.url.includes("/enter") && !req.cookies.carrotsession) {
+      // return NextResponse.redirect(`${req.nextUrl.origin}/enter`);
+      return NextResponse.redirect(new URL("/enter", req.url));
     }
   }
+  // }
 
   // return NextResponse.json({ ok: true });
 }
